624dc17dc965121508f893dc4e260c0e888b7c56
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-01 09:49:49 -0300

    inital commit

---
+++ ControleDeFluxo/controle_candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,19 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+	public static void main(String[] args) {
+		System.out.println("Processo Seletivo");
+		analisarCandidatos(2500.0);
+	}
+	
+	static void analisarCandidatos(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if (salarioBase > salarioPretendido) {
+			System.out.println("Ligar para candidato");
+		} else if (salarioBase == salarioPretendido) {
+			System.out.println("Ligar para o candidato com contra proposta");
+		} else  {
+			System.out.println("Aguardando o resultado dos demais candidatos");
+		}
+	}
+}
