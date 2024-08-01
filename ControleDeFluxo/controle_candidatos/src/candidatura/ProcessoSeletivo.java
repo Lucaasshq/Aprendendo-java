2a33f6f585e5874c164f36da01118fb16eabd7b8
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-01 11:21:09 -0300

    .

--- ControleDeFluxo/controle_candidatos/src/candidatura/ProcessoSeletivo.java
+++ ControleDeFluxo/controle_candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,35 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("Processo Seletivo");
-		analisarCandidatos(2500.0);
+		selecaoCandidatos();
+		
+		
 	}
+	
+	static void selecaoCandidatos() {
+		String[] candidatos = {"FELIPE", "MARCIA,","JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+		
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase =  2000.0;
+		
+		while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretentido();
+			
+			System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+			if (salarioBase >= salarioPretendido) {
+				System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+				candidatosSelecionados++;
+			}
+			candidatoAtual++;
+		}
+	}
+	
 	
 	static void analisarCandidatos(double salarioPretendido) {
 		double salarioBase = 2000.0;
@@ -15,5 +40,12 @@ public class ProcessoSeletivo {
 		} else  {
 			System.out.println("Aguardando o resultado dos demais candidatos");
 		}
+	}
+	
+	
+	// Função que retorna uma valor entre 1800 - 2200 de forma aleatoria 
+	
+	static double valorPretentido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 }
